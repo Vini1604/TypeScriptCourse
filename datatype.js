f19dd6424af2d1ba851224fe850babb0ed908a93
Vini1604 <vinicius.oliveira1998@hotmail.com>
2023-02-18 22:17:43 -0300

    Aula tipo de dados até any

--- datatype.js
+++ datatype.js
@@ -1,5 +1,35 @@
 "use strict";
-var meuNome;
+let meuNome;
 meuNome = 'Vinicius';
 console.log(meuNome);
-var minhaIdade;
+let minhaIdade;
+minhaIdade = 25;
+minhaIdade = 25.5;
+console.log(minhaIdade);
+let isMale = true;
+console.log(isMale);
+let familyNames;
+familyNames = ['Member1', 'Member2', 'Member3'];
+let familyAge;
+familyAge = [25, 17, 48, 53, 72];
+let younger30 = familyAge.filter(x => x < 30);
+let teen = familyAge.find(x => x === 17);
+let total = familyAge.reduce((acc, x) => acc + x);
+console.log(familyNames);
+console.log(familyAge);
+console.log(younger30);
+console.log(teen);
+console.log(total);
+var situacao;
+(function (situacao) {
+    situacao[situacao["Pending"] = 0] = "Pending";
+    situacao[situacao["Cancelled"] = 1] = "Cancelled";
+    situacao[situacao["Finished"] = 2] = "Finished";
+})(situacao || (situacao = {}));
+let situacaoProd = situacao.Finished;
+let tuple;
+function swapNumbers(n1, n2) {
+    return [n2, n1];
+}
+let returnFunction = swapNumbers(4, 5);
+console.log(returnFunction);
--- datatype.ts
+++ datatype.ts
@@ -5,3 +5,46 @@ meuNome = 'Vinicius';
 console.log(meuNome);
 
 let minhaIdade: number;
+minhaIdade = 25;
+minhaIdade = 25.5;
+console.log(minhaIdade);
+
+let isMale: boolean = true;
+console.log(isMale);
+
+let familyNames: Array<string>;
+familyNames = ['Member1', 'Member2', 'Member3'];
+
+let familyAge: number[];
+familyAge = [25, 17, 48, 53, 72];
+
+let younger30 = familyAge.filter(x => x < 30);
+let teen = familyAge.find(x => x === 17);
+let total = familyAge.reduce((acc, x) => acc + x);
+
+console.log(familyNames);
+console.log(familyAge);
+console.log(younger30);
+console.log(teen);
+console.log(total);
+
+enum situacao {
+    Pending,
+    Cancelled,
+    Finished
+}
+
+let situacaoProd:situacao = situacao.Finished;
+
+let tuple:[firstNumber:number, secondNumber:number]
+
+function swapNumbers(n1:number, n2:number):[number, number]{
+    return [n2, n1];
+}
+
+let returnFunction = swapNumbers(4,5);
+console.log(returnFunction);
+
+let qualquerUm: any;
+qualquerUm = 'Ola';
+qualquerUm = 10;
